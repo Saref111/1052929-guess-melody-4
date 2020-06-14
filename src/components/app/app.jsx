bc288e549a3285d35106bb17f11342254e4cf494
Saref111 <saref012@gmail.com>
2020-06-14 20:31:31 +0300

    Added propTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
-const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {mistakes} = props;
+const App = ({mistakes}) => {
   return (
     <WelcomeScreen mistakes={mistakes} />
   );
+};
+
+App.propTypes = {
+  mistakes: PropTypes.string.isRequired
 };
 
 export default App;
