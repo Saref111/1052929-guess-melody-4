c52899c5c68798c19489d69ed5e483fa4bb8a622
Saref111 <saref012@gmail.com>
2020-07-01 18:13:14 +0300

    Finish with hoc

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,53 +1,45 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class ArtistQuestionScreen extends PureComponent {
-  constructor(props) {
-    super(props);
+const ArtistQuestionScreen = (props) => {
 
-    this.state = {
-      isPlaying: true,
-    };
-  }
+  const {onAnswer, question, renderPlayer} = props;
+  const {type, song, answers} = question;
 
-  render() {
-    const {onAnswer, question, renderPlayer} = this.props;
-    const {type, song, answers} = question;
-
-    return (
-      <section className="game__screen">
-        <h2 className="game__title">Кто исполняет эту песню?</h2>
-        <div className="game__track">
-          <div className="track">
-            {renderPlayer(song.src, 0)}
-          </div>
+  return (
+    <section className="game__screen">
+      <h2 className="game__title">Кто исполняет эту песню?</h2>
+      <div className="game__track">
+        <div className="track">
+          {renderPlayer(song.src, 0)}
         </div>
+      </div>
 
-        <form className={`game__${type}`}>
-          {answers.map((answer, i) => {
-            return (
-              <div key={`${answer.picture}-${i}`} className="artist">
-                <input className="artist__input visually-hidden"
-                  type="radio" name="answer"
-                  value={`artist-${i}`}
-                  id={`answer-${i}`}
-                  onChange={(evt) => {
-                    evt.preventDefault();
+      <form className={`game__${type}`}>
+        {answers.map((answer, i) => {
+          return (
+            <div key={`${answer.picture}-${i}`} className="artist">
+              <input className="artist__input visually-hidden"
+                type="radio" name="answer"
+                value={`artist-${i}`}
+                id={`answer-${i}`}
+                onChange={(evt) => {
+                  evt.preventDefault();
 
-                    onAnswer(question, answer);
-                  }}/>
-                <label className="artist__name" htmlFor={`answer-${i}`}>
-                  <img className="artist__picture" src={answer.picture} alt={answer.artist} />
-                  {answer.artist}
-                </label>
-              </div>
-            );
-          })}
-        </form>
-      </section>
-    );
-  }
-}
+                  onAnswer(question, answer);
+                }}/>
+              <label className="artist__name" htmlFor={`answer-${i}`}>
+                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                {answer.artist}
+              </label>
+            </div>
+          );
+        })}
+      </form>
+    </section>
+  );
+
+};
 
 ArtistQuestionScreen.propTypes = {
   renderPlayer: PropTypes.func.isRequired,
